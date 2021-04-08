a09d1030ef96dc24f5d3d0bd73cef461892129d6
HassanBahati <mukisabahati@gmail.com>
2021-04-08 11:25:01 +0300

    created body component

---
+++ demo1/src/components/BodyComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>this is the body</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BodyComponent",
+};
+</script>
+
+<style scoped>
+</style>
--- demo1/src/components/Home.vue
+++ demo1/src/components/Home.vue
@@ -2,15 +2,18 @@
     <div>
         <!-- <h1> Welcome to home Component </h1> -->
         <Header/>
+        <BodyComponent/>
     </div>
 </template>
 
 <script>
 import Header from './Header.vue'
+import BodyComponent from './BodyComponent.vue'
     export default{
         name:'Home',
         components: {
             Header,
+            BodyComponent
         }
     }
 </script>
