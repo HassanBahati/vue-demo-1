017d60fa6c3574d44cd9b104d2d10441428790a2
HassanBahati <mukisabahati@gmail.com>
2021-04-08 10:21:59 +0300

    initial commit

---
+++ demo1/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <HomePage/>
+    </div>
+</template>
+
+<script setup>
+import HomePage from './components/Home.vue';
+
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style>
+
+</style>
